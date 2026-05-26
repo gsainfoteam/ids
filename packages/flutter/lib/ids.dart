aa5e719ff3ee519548944ba1f9b54dcb40892e68
Rhseung <ryu@rhseung.me>
2026-05-26 22:13:29 +0900

    fix: remove IdsSpacing dependency and fix widget test
    IDS-1

--- packages/flutter/lib/ids.dart
+++ packages/flutter/lib/ids.dart
@@ -1,7 +1,6 @@
 library ids;
 
 export 'tokens/ids_enums.dart';
-export 'tokens/ids_spacing.dart';
 export 'tokens/ids_motion.dart';
 export 'tokens/ids_typography.dart';
 export 'tokens/ids_color_tokens.dart';
